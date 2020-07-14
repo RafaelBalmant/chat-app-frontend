20fa7dc75a8fd17868998e773716e1619fb8222e
balmantrafael <balmantrafael@gmail.com>
2020-07-14 10:41:14 -0300

    created a new modal to set username

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import { Input, Button } from 'reactstrap';
 import io from 'socket.io-client';
 import uuid from 'uuid/dist/v4';
+import Swal from 'sweetalert2';
 
 const socket = io('https://chat-app-backend-b4tut4.herokuapp.com/');
 const id = uuid();
 function App() {
   const [messages, setMessages] = useState([]);
-
+  const [username, setUserName] = useState(['']);
   const [message, updateMessage] = useState('');
 
   useEffect(() => {
@@ -26,38 +27,63 @@ function App() {
     event.preventDefault();
     if (message.trim()) {
       socket.emit('chat.message', {
+        username,
         id,
         message,
       });
       updateMessage('');
     }
   };
-  console.log(message);
+
   const handlerInputMessage = (event) => updateMessage(event.target.value);
 
+  const insertUserName = useCallback(async () => {
+    await Swal.fire({
+      title: 'Insira o nome do usuário',
+      input: 'text',
+      showCancelButton: true,
+      inputValidator: (value) => {
+        setUserName(value);
+        if (!value) {
+          return 'Sinto muito, você precisa de um nome';
+        }
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    insertUserName();
+  }, []);
+
   return (
-    <div className="container-fluid h-100">
-      <div className="row h-100 ">
-        <div className="col-12">
-          {messages.map((value, i) => (
-            <div className={`d-flex m-2 flex-column ${value.id === id ? ' align-items-end' : ''}`}>
-              <p>
-                {`${value.id} diz:`}
-              </p>
-              <p>
-                {value.message}
-              </p>
+    <>
+      {
+      username.length > 1 && (
+        <div className="container-fluid h-100">
+          <div className="row h-100 ">
+            <div className="col-12">
+              {messages.map((value, i) => (
+                <div className={`d-flex m-2 flex-column ${value.id === id ? ' align-items-end' : ''}`}>
+                  <p>
+                    {`${value.username} diz:`}
+                  </p>
+                  <p>
+                    {value.message}
+                  </p>
+                </div>
+              ))}
             </div>
-          ))}
+            <div className="col-12 d-flex align-items-end">
+              <form onSubmit={updateMessageCallback} className="d-flex w-100 justify-content-between m-2">
+                <Input type="textarea" value={message} onChange={handlerInputMessage} />
+                <Button className="ml-2" type="submit">Enviar</Button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="col-12 d-flex align-items-end">
-          <form onSubmit={updateMessageCallback} className="d-flex w-100 justify-content-between m-2">
-            <Input type="textarea" value={message} onChange={handlerInputMessage} />
-            <Button className="ml-2" type="submit">Enviar</Button>
-          </form>
-        </div>
-      </div>
-    </div>
+      )
+    }
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'sweetalert2/src/sweetalert2.scss';
 
 ReactDOM.render(
   <React.StrictMode>
